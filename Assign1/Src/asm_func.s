5512e6e063ae77ff25ed68deb0ffce4fb6380eef
wen xi <tngwenxi@gmail.com>
2025-02-25 17:57:36 +0800

    Fix comments

--- Assign1/Src/asm_func.s
+++ Assign1/Src/asm_func.s
@@ -27,9 +27,9 @@
 @      Reused to store #12 for SUB operation, before LOOP_COUNT
 @ R2 - Exit
 @ R3 - Result
-@ R4 - Size of building (number_of_loops_left index!)
+@ R4 - Size of building (number_of_loops_left counter)
 @ R5 - Sum of entry cars
-@ R6 - Size of entry (number_of_loops_left index!) and
+@ R6 - Size of entry (number_of_loops_left counter) and
 @      Temp register used in LOOP_COUNT to load building[A][B]
 @      and store back to result[A][B] and
 @      Temp register used to store available slots in each section
@@ -47,7 +47,7 @@ asm_func:
 	LDR R4, [R3]
 	LDR R5, [R3, #4]
 
-	MUL R4, R5 // index and size of 1D buildingArr
+	MUL R4, R5 //  size of building (number_of_loops_left counter)
 
 	// Calculate total sum here
 	LDR R5, [R1], #4
@@ -61,12 +61,13 @@ asm_func:
 	ADD R5, R6
 
 	MOV R1, #MAX_CAP
-	// R5 = total sum, R6 & R7 useless, R4 is index
+	// R5 = total sum, R6 & R7 can be reused, R4 is number_of_loops_left 
+
 	LOOP_COUNT:
 		LDR R6, [R0], #4 //store current section in buildingArr to R6
 		LDR R7, [R2], #4 //store current section in exitArr to R7
 		SUB R6, R1, R6 //R1 == 12
-		//R6 IS AVAILABLE slots in section
+		//R6 is number of available slots in section
 		CMP R6, R5
 		ITT LE //if available slot <= total sum
 			//if true then
@@ -74,15 +75,15 @@ asm_func:
 			MOVLE R6, R1
 		ITTT GT
 			// else then
-			SUBGT R6, R1, R6 //make R6 current used slot in building[a][b]
+			SUBGT R6, R1, R6 //make R6 current numer of used slots in building[a][b]
 			ADDGT R6, R5
 			MOVGT R5, #0
 
-		//R6 is current updated current section, proceed to exiting cars
+		//R6 is current section updated, proceed to exiting cars
 		SUB R6, R7
 		STR R6, [R3], #4
 
-		SUBS R4, #1 // decrement number_of_loops_left index,
+		SUBS R4, #1 // decrement number_of_loops_left counter,
 		            // exit loop when R4 == 1 at before this line
 		            // R4 == 0 after this line!
 		BGT LOOP_COUNT
